f4325ade6ac172d29f8bdcf4789339ee87b4f3c1
Patrick Eads <patrick.eads@dartmouth.edu>
2017-08-03 13:47:41 -0400

    Made the code a little prettier.

--- linear_regression.py
+++ linear_regression.py
@@ -4,24 +4,31 @@ import statsmodels.api as sm
 import seaborn as sns
 import matplotlib.pyplot as plt
 import urllib2
+import functools as ft
 
-def normalize_dates_to(basedate, date):
+def normalize_date_to_base(date, basedate):
 	try:
 		return (pd.to_datetime(date) - basedate).days
 	except (pd._libs.tslib.OutOfBoundsDatetime, ValueError):
-		return normalize_dates_to(basedate, pd.Timestamp.min)
+		return normalize_date_to_base(pd.Timestamp.min, basedate)
+def filter_outliers(row):
+	return row['CheckCashed'] >= 0 and row['Approved'] >= 0 \
+		and row['CheckCashed'] < row['Approved'] \
+		and row['NFAItem'] == 'Suppressor' \
+		and row['Approved'] - row['CheckCashed'] >= 14 \
+		and row['FormType'] is not "Form 3 To Dealer"
 
+basedate = pd.Timestamp('2016-01-01')
+norm = ft.partial(normalize_date_to_base, basedate = basedate)
 url = "http://www.nfatracker.com/wp-content/themes/smartsolutions/inc/export/"
 response = urllib2.urlopen(url)
 df = pd.read_csv(response, header = 0)
-basedate = pd.Timestamp('2016-01-01')
-today = pd.to_datetime('today')
+
 
 #Filter data
-df['CheckCashed'] = df['CheckCashed'].apply(lambda x: normalize_dates_to(basedate, x))
-df['Approved'] = df['Approved'].apply(lambda x: normalize_dates_to(basedate, x))
-
-df = df[df.apply(lambda x: x['CheckCashed'] >= 0 and x['Approved'] >= 0 and x['CheckCashed'] < x['Approved'] and x['NFAItem'] == 'Suppressor', axis = 1)]
+df['CheckCashed'] = df['CheckCashed'].apply(norm)
+df['Approved'] = df['Approved'].apply(norm)
+df = df[df.apply(filter_outliers, axis = 1)]
 
 #Create model
 X = df['CheckCashed']
@@ -31,6 +38,7 @@ predictions = model.predict(X)
 
 #Plot linear regression
 sns.lmplot(x='CheckCashed',y='Approved',data=df,fit_reg=True) 
+#sns.regplot(x='CheckCashed',y='Approved', data=df, order=2)
 plt.show()
 
 
