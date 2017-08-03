f8ba68a812a556410e7904b379006fb287b9982b
Patrick Eads <patrick.eads@dartmouth.edu>
2017-08-03 12:29:46 -0400

    Now has more appropriate function name.

--- linear_regression.py
+++ linear_regression.py
@@ -5,6 +5,12 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import urllib2
 
+def normalize_dates_to(basedate, date):
+	try:
+		return (pd.to_datetime(date) - basedate).days
+	except (pd._libs.tslib.OutOfBoundsDatetime, ValueError):
+		return normalize_dates_to(basedate, pd.Timestamp.min)
+
 url = "http://www.nfatracker.com/wp-content/themes/smartsolutions/inc/export/"
 response = urllib2.urlopen(url)
 df = pd.read_csv(response, header = 0)
@@ -12,14 +18,8 @@ basedate = pd.Timestamp('2016-01-01')
 today = pd.to_datetime('today')
 
 #Filter data
-def filter_dates_before(basedate, date):
-	try:
-		return (pd.to_datetime(date) - basedate).days
-	except (pd._libs.tslib.OutOfBoundsDatetime, ValueError):
-		return filter_dates_before(basedate, pd.Timestamp.min)
-
-df['CheckCashed'] = df['CheckCashed'].apply(lambda x: filter_dates_before(basedate, x))
-df['Approved'] = df['Approved'].apply(lambda x: filter_dates_before(basedate, x))
+df['CheckCashed'] = df['CheckCashed'].apply(lambda x: normalize_dates_to(basedate, x))
+df['Approved'] = df['Approved'].apply(lambda x: normalize_dates_to(basedate, x))
 
 df = df[df.apply(lambda x: x['CheckCashed'] >= 0 and x['Approved'] >= 0 and x['CheckCashed'] < x['Approved'] and x['NFAItem'] == 'Suppressor', axis = 1)]
 
