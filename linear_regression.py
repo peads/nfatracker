30b21ba9c903a0ea21a5af151ba6e65545ca12da
Patrick Eads <patrick.eads@dartmouth.edu>
2017-08-08 16:41:57 -0400

    Changed method to OLS prediction. However, this breaks plotting.

--- linear_regression.py
+++ linear_regression.py
@@ -10,6 +10,7 @@ import functools as ft
 from argparse import ArgumentParser
 from statsmodels.graphics import utils
 from statsmodels.tools.tools import maybe_unwrap_results
+from statsmodels.sandbox.regression.predstd import wls_prediction_std
 
 
 def normalize_date_to_base(date, basedate):
@@ -19,11 +20,9 @@ def normalize_date_to_base(date, basedate):
         return normalize_date_to_base(pd.Timestamp.min, basedate)
 
 
-def filter_outliers(row, itemType):
-    return row['CheckCashed'] >= 0 and row['Approved'] >= 0 \
-           and row['CheckCashed'] < row['Approved'] \
-           and row['NFAItem'] == itemType \
-           and row['Approved'] - row['CheckCashed'] >= 14 \
+def filter_outliers(row, item_type):
+    return row['CheckCashed'] >= 0 and row['Approved'] - row['CheckCashed'] >= 14 \
+           and row['NFAItem'] == item_type \
            and row['FormType'] is not "Form 3 To Dealer"
 
 
@@ -37,45 +36,44 @@ def parse_args():
     parser.add_argument("-t", "--nfa-item-type", dest="type", default='Suppressor',
                         choices=['Suppressor', 'SBR', 'SBS', 'MG', 'AOW'],
                         help="Type of NFA item on which to resrict data.", metavar="TYPE")
-    parser.add_argument('--plot-regression', dest='plot', action='store_true',
-                        help="Plot a linear regression of data normalized around BASEDATE.")
-    parser.add_argument('--no-plot-regression', dest='plot', action='store_false',
-                        help="Default: does not plot linear regression.")
+    # parser.add_argument('--plot-regression', dest='plot', action='store_true',
+    #                     help="Plot a linear regression of data normalized around BASEDATE.")
+    # parser.add_argument('--no-plot-regression', dest='plot', action='store_false',
+    #                     help="Default: does not plot linear regression.")
     parser.add_argument("-v", '--verbose', dest='verbose', action='store_true',
                         help="Print verbose information during execution.")
     parser.add_argument('--no-verbose', dest='verbose', action='store_false',
                         help="Default: does not print verbose information during execution.")
-    parser.set_defaults(plot=False)
+    parser.set_defaults(verbose=False)
     return parser.parse_args()
 
-
-def plot_fit(results, exog_idx, y_true=None, ax=None, fittedname=None, **kwargs):
-    fig, ax = utils.create_mpl_ax(ax)
-
-    exog_name, exog_idx = utils.maybe_name_or_idx(exog_idx, results.model)
-    results = maybe_unwrap_results(results)
-
-    # maybe add option for wendog, wexog
-    y = results.model.endog
-    x1 = results.model.exog[:, exog_idx]
-    x1_argsort = np.argsort(x1)
-    y = y[x1_argsort]
-    x1 = x1[x1_argsort]
-
-    if not y_true is None:
-        ax.plot(x1, y_true[x1_argsort], '-', label='True values')
-    title = 'Fitted values versus %s' % exog_name
-
-    # prstd, iv_l, iv_u = wls_prediction_std(results)
-    ax.plot(x1, results.fittedvalues[x1_argsort], '-',
-            label='fitted' if fittedname is None else fittedname, **kwargs)
-
-    ax.set_title(title)
-    ax.set_xlabel(exog_name)
-    ax.set_ylabel(results.model.endog_names)
-    ax.legend(loc='best', numpoints=1)
-
-    return fig
+#
+# def plot_fit(results, exog_idx, y_true=None, ax=None, fittedname=None, **kwargs):
+#     fig, ax = utils.create_mpl_ax(ax)
+#
+#     exog_name, exog_idx = utils.maybe_name_or_idx(exog_idx, results.model)
+#     results = maybe_unwrap_results(results)
+#
+#     # maybe add option for wendog, wexog
+#     y = results.model.endog
+#     x1 = results.model.exog[:, exog_idx]
+#     x1_argsort = np.argsort(x1)
+#     y = y[x1_argsort]
+#     x1 = x1[x1_argsort]
+#
+#     if not y_true is None:
+#         ax.plot(x1, y_true[x1_argsort], 'g-', label='True values')
+#     title = 'Fitted values versus %s' % exog_name
+#
+#     ax.plot(x1, results.fittedvalues[x1_argsort], 'g-',
+#             label='fitted' if fittedname is None else fittedname, **kwargs)
+#
+#     ax.set_title(title)
+#     ax.set_xlabel(exog_name)
+#     ax.set_ylabel(results.model.endog_names)
+#     ax.legend(loc='best', numpoints=1)
+#
+#     return fig
 
 
 def generate_dataframe(url):
@@ -85,33 +83,50 @@ def generate_dataframe(url):
 
 
 def filter_dataframe(df, normalize_dates, filter_outliers):
+    df.dropna(subset=['CheckCashed', 'Approved', 'NFAItem'], how='any', inplace=True)
     df['CheckCashed'] = df['CheckCashed'].apply(normalize_dates)
     df['Approved'] = df['Approved'].apply(normalize_dates)
     return df[df.apply(filter_outliers, axis=1)]
 
 
-def plot_model_and_prediction(df, models, prediction, date):
-    # Plot data
-    ax = df.plot(x='CheckCashed', y='Approved', kind='scatter')
-
-    # Plot models
-    for i, model in enumerate(models):
-        plot_fit(model, 0, ax=ax, fittedname="Fitted %d" % (i+1))
-    ax_plot = ft.partial(ax.plot, marker='d')
-    ax_annotate = ft.partial(ax.annotate, s='Prediction', textcoords='data')
-
-    # Plot prediction
-    xy = (date, prediction)
-    ax_plot(*xy)
-    ax_annotate(xy=xy)
-
-    plt.show()
-
+# def plot_model_and_prediction(df, results, prediction, date):
+#     # Plot data
+#     ax = df.plot(x='CheckCashed', y='Approved', kind='scatter')
+#
+#     # Plot models
+#     for i, result in enumerate(results):
+#         _, iv_l, iv_u = wls_prediction_std(result)
+#         _, exog_idx = utils.maybe_name_or_idx(None, result.model)
+#         x = result.model.exog[:, exog_idx]
+#         ax.plot(x, iv_u, 'r-')
+#         ax.plot(x, iv_l, 'r-')
+#         plot_fit(result, 0, ax=ax, fittedname="Fitted %d" % (i + 1))
+#
+#     # Plot prediction
+#     xy = (date, prediction)
+#     ax.plot(*xy, marker='d', color='y')
+#     ax.annotate(xy=xy, s='Prediction', textcoords='data')
+#
+#     plt.show()
 
 def generate_models(df):
     y, X = df['Approved'], df['CheckCashed']
-    return (sm.OLS(y, X), sm.RLM(y, X, M=sm.robust.norms.HuberT()), sm.RLM(y, X, M=sm.robust.norms.AndrewWave()))
+    ols_model = sm.OLS(y, X, missing='raise')
+    X = sm.add_constant(X, has_constant='raise')
+    ols_model_intercept = sm.OLS(y, X, missing='raise')
+    # ols_resid = ols_model.fit().resid
+    # resid_fit = sm.OLS(ols_resid[1:], sm.add_constant(ols_resid[:-1])).fit()
+    # rho = resid_fit.params[1]
+    # order = toeplitz(range(len(ols_resid)))
+    # sigma = rho**order
+    # gls_model = sm.GLS(y, X, sigma=sigma)
+    return ols_model, ols_model_intercept
 
+def predict_scalar_or_not(result, date):
+    try:
+        return result.predict((1, date))
+    except ValueError:
+        return result.predict(date)
 
 def main():
     args = parse_args()
@@ -119,10 +134,11 @@ def main():
 
     # Create partially applied functions
     norm = ft.partial(normalize_date_to_base, basedate=basedate)
-    outliers = ft.partial(filter_outliers, itemType=args.type)
+    outliers = ft.partial(filter_outliers, item_type=args.type)
     convert_back_to_datetime = ft.partial(pd.to_datetime, origin=basedate, unit='D')
-
     date = norm(pd.Timestamp(args.date))
+    predict = ft.partial(predict_scalar_or_not, date=date)
+
 
     # Get data
     df = generate_dataframe("http://www.nfatracker.com/wp-content/themes/smartsolutions/inc/export/")
@@ -131,20 +147,21 @@ def main():
     df = filter_dataframe(df, norm, outliers)
 
     # Fit, print and plot as specified
-    models = tuple(map(lambda x: x.fit(), generate_models(df)))
-    predictions = tuple(map(lambda model: model.predict(date), models))
+    results = tuple(map(lambda x: x.fit(), generate_models(df)))
 
+    predictions = tuple(map(predict, results))
     if args.verbose:
-        for model, prediction in zip(models, tuple(map(np.vectorize(convert_back_to_datetime), predictions))):
-            print model.summary()
+        for result, prediction in zip(results, tuple(map(np.vectorize(convert_back_to_datetime), predictions))):
+            print result.summary()
             print 'Predicted approval date:', prediction
 
     prediction = np.mean(predictions)
 
     print 'Predicted approval date:', np.vectorize(convert_back_to_datetime)(prediction)
 
-    if args.plot:
-        plot_model_and_prediction(df, models, prediction, date)
+    # if args.plot:
+    #     plot_model_and_prediction(df, results, prediction, date)
+
 
 if __name__ == "__main__":
     main()
